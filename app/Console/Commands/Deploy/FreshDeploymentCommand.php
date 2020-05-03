89a96ece53a795c3028269832adf30d408edd03e
Martin Juul <code@juul.xyz>
2020-05-03 17:11:26 +0200

    [Feature]: Add deploy:fresh command

---
+++ app/Console/Commands/Deploy/FreshDeploymentCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands\Deploy;
+
+use Illuminate\Console\Command;
+
+class FreshDeploymentCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deploy:fresh {--dev}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Handles migrations and optimisations';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh', [
+            '--force' => true,
+        ]);
+
+        $this->call('optimize:clear');
+        $this->call('event:clear');
+        $this->call('view:clear');
+    }
+}
