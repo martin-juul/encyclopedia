29db3dbbd96484e42d19e79a286966e6e7935ff1
Martin Juul <code@juul.xyz>
2020-05-05 16:24:03 +0200

    Change default font to Work Sans

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
   .js('resources/js/dashboard', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
-  .sass('resources/sass/dashboard.scss', 'public/css');
+  .sass('resources/sass/dashboard.scss', 'public/css')
+  .copyDirectory('resources/fonts', 'public/assets/fonts');
 
 mix.disableNotifications();
