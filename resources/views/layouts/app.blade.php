fb3c3de2f11e136405a93cfbeb3c70da3af1d197
Martin Juul <code@juul.xyz>
2020-05-05 16:24:38 +0200

    Remove Nunito from <head>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,6 @@
 
     <title>@yield('title') - {{ config('app.name', 'Encyclopedia') }}</title>
 
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <link rel="dns-prefetch" href="//stackpath.bootstrapcdn.com">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
